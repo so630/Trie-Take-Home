e4852b75976e9f95ea789fb37c3ca7769c111bd8
so630 <73109255+so630@users.noreply.github.com>
2021-10-14 20:15:58 +0400

    m

---
+++ cli/README.md
@@ -0,0 +1,18 @@
+# Trie
+
+## Installation
+```bash
+git clone https://github.com/so630/trie-cli.git
+```
+then cd into the directory where you cloned and type
+```bash
+npm link
+```
+
+## Running the program
+
+type
+```bash
+trie --help
+```
+for help on commands
---
+++ cli/src/cli.js
@@ -0,0 +1,85 @@
+const axios = require('axios')
+
+export function cli(args) {
+    let command = args[2];
+    let word = args[3];
+    switch (command) {
+        case 'add':
+            if (!word) {
+                console.log('No word specified');return
+            }
+            let link = 'https://agile-reaches-81103.herokuapp.com/add?q=' + word;
+            axios.post(link).then(function (response) {
+                console.log('successfully added ' + word + ' to trie');
+            }).catch(function (err) {
+                console.log(err)
+            })
+            break;
+        case 'all':
+            let link2 = 'https://agile-reaches-81103.herokuapp.com/all';
+            axios.get(link2).then(function (response) {
+                if (response.data.length == 0) {
+                    console.log('no data in trie');
+                } else {
+                    for (let i = 0; i < response.data.length; i++) {
+                        console.log(response.data[i]);
+                    }
+                }
+            }).catch(function (err) {
+                console.log(err);
+            })
+            break;
+        case 'delete':
+            if (!word) {
+                console.log('No word specified');return
+            }
+            let link3 = 'https://agile-reaches-81103.herokuapp.com/delete?q=' + word;
+            axios.post(link3).then((res) => {
+                if (res.data.message == 'word not found') {
+                    console.log('word not found');
+                } else {
+                    console.log('successfully deleted ' + word + ' from trie');
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+            break;
+        case 'find':
+            if (!word) {
+                console.log('No word specified');return
+            }
+            let link4 = 'https://agile-reaches-81103.herokuapp.com/query?q=' + word;
+            axios.get(link4).then((res) => {
+                if (res.data.length == 0) {
+                    console.log('no data in trie with suffix ' + word);
+                } else {
+                    for (let i = 0; i < res.data.length; i++) {
+                        console.log(res.data[i]);
+                    }
+                }
+            }).catch((err) => {
+                console.log(err);
+            })
+            break;
+        case 'has':
+            if (!word) {
+                console.log('No word specified');return
+            }
+            let link5 = 'https://agile-reaches-81103.herokuapp.com/has?q=' + word;
+            axios.get(link5).then((res) => {
+                console.log(res.data)
+            }).catch((err) => {
+                console.log(err);
+            })
+            break;
+        case '--help':
+            console.log("Here is what you can do: ");
+            console.log("1. Add keyword to the trie, command: trie add [keyword]");
+            console.log("2. Delete keyword from the trie, command: trie del [keyword]");
+            console.log("3. search for keyword in trie, command: trie has [keyword]");
+            console.log("4. Return list of autocomplete suggestion based on an input prefix, command: trie find [prefix]");
+            console.log("5. Display the words in the tree, command: trie all");
+            console.log("6. Quit, command: trie quit");
+            break;
+    }
+}
